45c2e74ac3e0bbfc05d0769c13332066d197f95e
jchang <jchang@ucls.uchicago.edu>
2016-05-31 13:29:58 -0500

    printing question to label

--- src/everyoneexplodesstoptalking/FXMLDocumentController.java
+++ src/everyoneexplodesstoptalking/FXMLDocumentController.java
@@ -10,47 +10,58 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.Group;
+import javafx.scene.control.*;
+import javafx.scene.Scene;
 
 /**
  *
  * @author csstudent
  */
 public class FXMLDocumentController implements Initializable {
+    
     @Override
     public void start(Stage stage){
-        //private Label label;
-        Group root = new Group();
-        Scene scene = new Scene(root, 260, 80);
+        
+
+   
+        VBox root = new Vbox(5);
+        
+        Label label = new Label("label");
+        TextArea text = new TextArea();
+        text.setPrefRowCount(10);
+        text.setPrefColumnCount(20);
+        text.setWrapText(true);
+        
+        Button printText = new Button("Prompt Question");
+        printText.setOnAction(e -> print(text));
+        
+        HBox buttonBox = new HBox(5, printText);
+        
+        root.getChildren().addAll(label, text, buttonBox);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Defuse this");
-
-        VBox vb = new VBox();
-        vb.setPadding(new Insets(10, 0, 0, 10));
-        vb.setSpacing(10);
-        HBox hb = new HBox();
-        hb.setSpacing(10);
-        hb.setAlignment(Pos.CENTER_LEFT);
-
-        Label label = new Label("keycode");
+        stage.show();
         //final PasswordField pb = new PasswordField();
-        TextField enterNumber = new TextField ();
-        enterNumber.setOnKeyPressed(new EventHandler <KeyEvent>(){
+        //TextField enterNumber = new TextField ();
+        //enterNumber.setOnKeyPressed(new EventHandler <KeyEvent>(){
             /* public void handle(KeyEvent e){
                 if(e.getCode() -- KeyCode.ENTER) {
                     //compare the textfield with the answer field.getText 
             }
         } */
+            
+            //private Label label;
+
+        
     }
-            }
-   
+}
     
  
          
@@ -65,10 +76,7 @@ public class FXMLDocumentController implements Initializable {
         // TODO
     }    
     
-    
-HBox hb = new HBox();
-hb.getChildren().addAll(label1, textField);
-hb.setSpacing(10);
+
 //override handle keyevent e, e.getcode --keycode.enter
 //https://www.youtube.com/watch?v=R8Qx_UiG18I
 //fxid is "enterNumber" http://code.makery.ch/blog/javafx-8-event-handling-examples/
