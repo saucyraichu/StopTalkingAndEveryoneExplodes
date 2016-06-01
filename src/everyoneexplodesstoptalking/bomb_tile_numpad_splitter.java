bcbc4bb618c5f333bf9f98eb924d83fed7686a86
jchang <jchang@ucls.uchicago.edu>
2016-06-01 13:30:40 -0500

    fixing

--- src/everyoneexplodesstoptalking/bomb_tile_numpad_splitter.java
+++ src/everyoneexplodesstoptalking/bomb_tile_numpad_splitter.java
@@ -5,13 +5,16 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+package everyoneexplodesstoptalking;
+
 public class bomb_tile_numpad_splitter extends bomb_tile_numpad
 {
     public bomb_tile_numpad_splitter()
     {
         
     }
-    public void question(){
+    public String question(){
+        
         String question = new String ("");
         String[] firstTemp = getDefuse().split(","); //Split the string by commas
         String[] tempInt = firstTemp[0].split(" "); //Take the first item in the array, split it by spaces
@@ -20,6 +23,6 @@ public class bomb_tile_numpad_splitter extends bomb_tile_numpad
         //Then put the array of ints and array of chars into string question formatted as a question
         question = "What is " + tempInt[0] + " " + tempChar[0] + " " + tempInt[1] + " " + tempChar[1] + " " + tempInt[2] + " " + tempChar[2] + " " + tempInt[3] + "?"; 
         //need to format strings into chars and ints
-        System.out.println(question);
+        return question;
     }
 }
